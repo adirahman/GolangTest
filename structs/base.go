821aedca171b6fcdefa3ec56f586d825c9c2aa3d
adi <rahmanadi9@gmail.com>
2019-04-03 20:14:17 +0700

    main class

---
+++ structs/base.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"payfazztest/config"
+	"payfazztest/controllers"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Credential struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+	db := config.DBInit()
+	inDB := &controllers.InDB{DB: db}
+
+	router := gin.Default()
+
+	//Person
+	router.POST("/login", loginHandler)
+	router.GET("/person/:id", auth, inDB.GetPerson)
+	router.GET("/persons", auth, inDB.GetPersons)
+	router.POST("/person", auth, inDB.CreatePerson)
+	router.PUT("/person", auth, inDB.UpdatePerson)
+	router.DELETE("/person/:id", auth, inDB.DeletePerson)
+
+	//Company
+	router.GET("/company/:id", auth, inDB.GetCompany)
+	router.GET("/companies", auth, inDB.GetCompanies)
+	router.POST("/company", auth, inDB.CreateCompany)
+	router.PUT("/company", auth, inDB.UpdateCompany)
+	router.DELETE("/company/:id", auth, inDB.DelteCompany)
+
+	//friends
+	router.GET("/friends", auth, inDB.GetFriends)
+	router.POST("/friend", auth, inDB.CreateFriends)
+
+	//work history
+	router.GET("/workhistories/:id", auth, inDB.GetWorkHistory)
+	router.POST("/workhostory", auth, inDB.CreateWork)
+
+	router.Run(":3000")
+}
+
+func loginHandler(c *gin.Context) {
+	var user Credential
+	err := c.Bind(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": "can't bind struct",
+		})
+	}
+	if user.Username != "myname" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"status":  http.StatusUnauthorized,
+			"message": "wrong username or password",
+		})
+	} else {
+		if user.Password != "myname123" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"status":  http.StatusUnauthorized,
+				"message": "wrong username or password",
+			})
+		}
+	}
+	sign := jwt.New(jwt.GetSigningMethod("HS256"))
+	token, err := sign.SignedString([]byte("secret"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		c.Abort()
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+	})
+}
+
+func auth(c *gin.Context) {
+	tokenString := c.Request.Header.Get("Authorization")
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if jwt.GetSigningMethod("HS256") != token.Method {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte("secret"), nil
+	})
+
+	// if token.Valid && err == nil {
+	if token != nil && err == nil {
+		fmt.Println("token verified")
+	} else {
+		result := gin.H{
+			"message": "not authorized",
+			"error":   err.Error(),
+		}
+		c.JSON(http.StatusUnauthorized, result)
+		c.Abort()
+	}
+}
